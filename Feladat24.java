b27e2abe3aeda4cb87d909c05b5155587a0d1e39
gotz-patrik-katolikuskeri <99012981+gotz-patrik-katolikuskeri@users.noreply.github.com>
2022-02-25 11:42:19 +0100

    Add files via upload

--- Feladat24.java
+++ Feladat24.java
@@ -2,16 +2,14 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
-
+public class Feladat24 {
     public static void main(String[] args) {
-	// write your code here
         Scanner szkenner = new Scanner(System.in);
         System.out.print("Kérem a pozitív egész számot: ");
         int szam = szkenner.nextInt();
         int szamlalo = 0;
         while (szamlalo != szam){
-            System.out.printf("%d\t",szamlalo++);
+            System.out.printf("%d",szamlalo++);
         }
     }
 }
---
+++ Feladat25.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Feladat25 {
+    public static void main(String[] args) {
+        Scanner szkenner = new Scanner(System.in);
+        
+    }
+}
